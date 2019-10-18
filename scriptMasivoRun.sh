e89ce141ea53f4dc2d7c51982331924ddc8664d4
dayala <dylan.ayala@sondeos.com.ar>
2019-10-18 15:18:19 -0300

    se elimina el sh

--- scriptMasivoRun.sh
+++
@@ -1,9 +0,0 @@
-MyPid=$(pgrep -f "/usr/bin/python3 manage.py ProcessMasivo")
-
-if [ "$MyPid" != "" ];
-    then
-    echo "$MyPid corriendo"
-else
-    echo "$MyPid no corriendo"
-    /usr/bin/python3 /appuser/proyectos/stock-api/manage.py ProcessMasivo
-fi
